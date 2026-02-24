6674e07341f80248ec8d5e4ab94e5e0b26218d68
rgm <git@rushd.xyz>
2026-02-24 14:28:56 +0000

    ResponseEntity exception handler with BindingResult.

--- src/main/java/xyz/rushd/validation/exception/GlobalExceptionHandler.java
+++ src/main/java/xyz/rushd/validation/exception/GlobalExceptionHandler.java
@@ -1,18 +1,25 @@
 package xyz.rushd.validation.exception;
 
-import org.springframework.http.HttpStatus;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public String handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-    return ex.getMessage();
+  public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+      String fieldName = fieldError.getField();
+      String errorMessage = fieldError.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    }
+    return ResponseEntity.badRequest().body(errors);
   }
-
 }
