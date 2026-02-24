a2983de0186615ff3d7822e114168ea3e3e8d4db
rgm <git@rushd.xyz>
2026-02-24 13:07:28 +0000

    Created GlobalExceptionHandler.java.

---
+++ src/main/java/xyz/rushd/validation/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package xyz.rushd.validation.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public String handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    return ex.getMessage();
+  }
+
+}
