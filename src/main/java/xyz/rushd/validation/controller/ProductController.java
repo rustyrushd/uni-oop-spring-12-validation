75242a2b9026e03607b5ff7603b0be5ea02c0b10
rgm <git@rushd.xyz>
2026-02-24 12:16:36 +0000

    Created ProductController.java.

---
+++ src/main/java/xyz/rushd/validation/controller/ProductController.java
@@ -0,0 +1,34 @@
+package xyz.rushd.validation.controller;
+
+import jakarta.validation.Valid;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.rushd.validation.model.Product;
+import xyz.rushd.validation.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+  private final ProductService productService;
+
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Product> addProduct(@Valid @RequestBody Product product) {
+    Product saved =  productService.addProduct(product);
+    return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Product>> getAllProducts() {
+    return ResponseEntity.ok(productService.getAllProducts());
+  }
+}
